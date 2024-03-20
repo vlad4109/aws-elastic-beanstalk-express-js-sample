303328004556167e7c1dc22dac584758f6bc02c3
Munteanu Vlad <vladmicoldo@Munteanus-MacBook-Pro.local>
2024-03-20 16:17:07 +0200

    message change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Making the first change into this cloned repo!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
