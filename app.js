6fde5e393f20df4470906b5a403479507a5f2c0d
vlad4109 <vladmicoldo@Munteanus-MacBook-Pro.local>
2024-03-21 12:58:34 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Making the first change into this cloned repo!'));
+app.get('/', (req, res) => res.send('Making the second change into this cloned repo!\n Content needs to be reviewed and approved'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
